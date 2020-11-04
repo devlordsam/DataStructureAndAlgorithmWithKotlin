890179324358b508bae530be860f6ce723925773
LordSam <49715934+LordSam07@users.noreply.github.com>
2020-11-04 13:42:46 +0530

    Re Updated

--- app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/DataStructureActivity.kt
+++ app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/DataStructureActivity.kt
@@ -3,7 +3,7 @@ package com.lordsam.datastructureandalgorithmwithkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DataStrucutreActivity : AppCompatActivity() {
+class DataStructureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_strucutre)
