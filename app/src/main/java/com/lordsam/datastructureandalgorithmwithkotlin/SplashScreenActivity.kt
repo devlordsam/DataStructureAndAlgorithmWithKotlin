2437e9fd2ca1efbb3a4c79870bdf6ddcb05bc9ec
LordSam <49715934+LordSam07@users.noreply.github.com>
2020-11-07 19:23:23 +0530

    Update Animation

--- app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/SplashScreenActivity.kt
@@ -1,7 +1,10 @@
 package com.lordsam.datastructureandalgorithmwithkotlin
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
 import com.airbnb.lottie.LottieAnimationView
@@ -11,12 +14,15 @@ class SplashScreenActivity : AppCompatActivity() {
     private lateinit var bg: ImageView
     private lateinit var txtAppName: TextView
     private lateinit var lottieAnimationView: LottieAnimationView
+    private lateinit var sharedPref: SharedPreferences
+    private var firstRun: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        sharedPref = getSharedPreferences("First Run", MODE_PRIVATE)
 
-        animate()
+        isFirstRun()
     }
 
     private fun animate() {
@@ -28,5 +34,21 @@ class SplashScreenActivity : AppCompatActivity() {
         bg.animate().translationY(-1600F).setDuration(1000).startDelay = 4000
         txtAppName.animate().translationY(1600F).setDuration(1000).startDelay = 4000
         lottieAnimationView.animate().translationY(1600F).setDuration(1000).startDelay = 4000
+    }
+
+    private fun isFirstRun() {
+
+        firstRun = sharedPref.getBoolean("isFirstRun", true)
+
+        if (firstRun!!) {
+            sharedPref.edit().putBoolean("isFirstRun", false).apply()
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            animate()
+            Handler().postDelayed({
+                startActivity(Intent(this, LoginActivity::class.java))
+            }, 3000)
+        }
+
     }
 }
