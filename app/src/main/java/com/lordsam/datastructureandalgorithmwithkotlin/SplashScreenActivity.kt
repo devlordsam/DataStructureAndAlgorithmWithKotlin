9bc524a03c73c325f6b25bbe8d12f1c28e239c3b
LordSam <49715934+LordSam07@users.noreply.github.com>
2020-11-07 18:45:36 +0530

    Update Animation

--- app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/lordsam/datastructureandalgorithmwithkotlin/SplashScreenActivity.kt
@@ -7,19 +7,26 @@ import android.widget.TextView
 import com.airbnb.lottie.LottieAnimationView
 
 class SplashScreenActivity : AppCompatActivity() {
-    lateinit var lottieAnimationView: LottieAnimationView
+
+    private lateinit var bg: ImageView
+    private lateinit var txtAppName: TextView
+    private lateinit var lottieAnimationView: LottieAnimationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val bg = findViewById<ImageView>(R.id.imageViewBackground)
-        val txtAppName = findViewById<TextView>(R.id.textViewSSTitle)
+        animate()
+    }
+
+    private fun animate() {
+
+        bg = findViewById(R.id.imageViewBackground)
+        txtAppName = findViewById(R.id.textViewSSTitle)
         lottieAnimationView = findViewById(R.id.lottie)
 
         bg.animate().translationY(-1600F).setDuration(1000).startDelay = 4000
         txtAppName.animate().translationY(1600F).setDuration(1000).startDelay = 4000
         lottieAnimationView.animate().translationY(1600F).setDuration(1000).startDelay = 4000
-
-
     }
 }
